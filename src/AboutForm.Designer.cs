ce19b6416097e6fdb60ab9ea489cbb12c7fe2d8a
Paweł Hofman <pawel@codetitans.pl>
2015-03-21 19:37:13 +0100

    Added AboutForm

---
+++ src/AboutForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace CodeTitans.Signature
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCodeTitans = new System.Windows.Forms.Label();
+            this.bttClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.projectLink = new System.Windows.Forms.LinkLabel();
+            this.twitterLink = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.issuesLink = new System.Windows.Forms.LinkLabel();
+            this.homeLink = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblCodeTitans);
+            this.panel1.Location = new System.Drawing.Point(-2, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(510, 124);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblCodeTitans
+            // 
+            this.lblCodeTitans.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCodeTitans.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCodeTitans.Location = new System.Drawing.Point(14, 46);
+            this.lblCodeTitans.Name = "lblCodeTitans";
+            this.lblCodeTitans.Size = new System.Drawing.Size(477, 42);
+            this.lblCodeTitans.TabIndex = 0;
+            this.lblCodeTitans.Text = "CodeTitans Signature";
+            this.lblCodeTitans.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bttClose
+            // 
+            this.bttClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bttClose.Location = new System.Drawing.Point(416, 223);
+            this.bttClose.Name = "bttClose";
+            this.bttClose.Size = new System.Drawing.Size(75, 23);
+            this.bttClose.TabIndex = 1;
+            this.bttClose.Text = "&Close";
+            this.bttClose.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.issuesLink);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.twitterLink);
+            this.groupBox1.Controls.Add(this.projectLink);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 127);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 90);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This is an open-source project created by";
+            // 
+            // projectLink
+            // 
+            this.projectLink.AutoSize = true;
+            this.projectLink.Location = new System.Drawing.Point(160, 50);
+            this.projectLink.Name = "projectLink";
+            this.projectLink.Size = new System.Drawing.Size(28, 13);
+            this.projectLink.TabIndex = 1;
+            this.projectLink.TabStop = true;
+            this.projectLink.Text = "here";
+            this.projectLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.projectLink_LinkClicked);
+            // 
+            // twitterLink
+            // 
+            this.twitterLink.AutoSize = true;
+            this.twitterLink.Location = new System.Drawing.Point(221, 26);
+            this.twitterLink.Name = "twitterLink";
+            this.twitterLink.Size = new System.Drawing.Size(155, 13);
+            this.twitterLink.TabIndex = 2;
+            this.twitterLink.TabStop = true;
+            this.twitterLink.Text = "Paweł Hofman (@CodeTitans).";
+            this.twitterLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.twitterLink_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Full source-code is available";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "with the public";
+            // 
+            // issuesLink
+            // 
+            this.issuesLink.AutoSize = true;
+            this.issuesLink.Location = new System.Drawing.Point(269, 50);
+            this.issuesLink.Name = "issuesLink";
+            this.issuesLink.Size = new System.Drawing.Size(64, 13);
+            this.issuesLink.TabIndex = 5;
+            this.issuesLink.TabStop = true;
+            this.issuesLink.Text = "bug-tracker.";
+            this.issuesLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.issuesLink_LinkClicked);
+            // 
+            // homeLink
+            // 
+            this.homeLink.AutoSize = true;
+            this.homeLink.Location = new System.Drawing.Point(12, 228);
+            this.homeLink.Name = "homeLink";
+            this.homeLink.Size = new System.Drawing.Size(94, 13);
+            this.homeLink.TabIndex = 3;
+            this.homeLink.TabStop = true;
+            this.homeLink.Text = "www.codetitans.pl";
+            this.homeLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.homeLink_LinkClicked);
+            // 
+            // AboutForm
+            // 
+            this.AcceptButton = this.bttClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.bttClose;
+            this.ClientSize = new System.Drawing.Size(503, 258);
+            this.Controls.Add(this.homeLink);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bttClose);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCodeTitans;
+        private System.Windows.Forms.Button bttClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel issuesLink;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel twitterLink;
+        private System.Windows.Forms.LinkLabel projectLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel homeLink;
+    }
+}
---
+++ src/AboutForm.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace CodeTitans.Signature
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void homeLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogHelper.StartURL("http://www.codetitans.pl");
+        }
+
+        private void twitterLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogHelper.StartURL("http://twitter.com/CodeTitans");
+        }
+
+        private void projectLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogHelper.StartURL("https://github.com/phofman/signature");
+        }
+
+        private void issuesLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogHelper.StartURL("https://github.com/phofman/signature/issues");
+        }
+    }
+}
